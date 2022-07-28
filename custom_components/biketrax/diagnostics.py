c9f6ef618dbb7b84b3c9963b7c1660d7700e9c1d
vlebourl <vlebourl@gmail.com>
2022-07-28 10:32:23 +0200

    add diagnostics.py

---
+++ custom_components/biketrax/diagnostics.py
@@ -0,0 +1,33 @@
+"""Diagnostics support for Tile."""
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.components.diagnostics import async_redact_data
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
+from homeassistant.core import HomeAssistant
+
+from . import BikeTraxData
+from .const import DOMAIN
+
+CONF_ALTITUDE = "altitude"
+CONF_UUID = "uuid"
+
+TO_REDACT = {
+    CONF_ALTITUDE,
+    CONF_LATITUDE,
+    CONF_LONGITUDE,
+    CONF_UUID,
+}
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+    data: BikeTraxData = hass.data[DOMAIN][entry.entry_id]
+
+    return async_redact_data(
+        {"bikes": [bt.as_dict() for bt in data.bikes.values()]}, TO_REDACT
+    )
